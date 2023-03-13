e56fb053d0e37bd319b91b62c0902a8b9b8c8e53
Denys Bzenko <dbzenko@kse.org.ua>
2023-03-13 22:50:01 +0200

    debug

--- Itname2/MapGenerator.cs
+++ Itname2/MapGenerator.cs
@@ -82,7 +82,8 @@
                 while (n > 1)
                 {
                     var k = rng.Next(n--);
-                    (array[n], array[k]) = (array[k], array[n]);
+                    array[n] = array[k];
+                    array[k] = array[n];
                 }
             }
         }
--- Itname2/Program.cs
+++ Itname2/Program.cs
@@ -4,6 +4,8 @@ var generator = new MapGenerator(new MapGeneratorOptions()
 {
     Height = 35,
     Width = 90,
+    Seed = 100,
+    Noise = (float)0.3
 });
 
 string[,] map = generator.Generate();
