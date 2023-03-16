f179e55f812554ba98c296b23840b118ff9f23f7
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-03-16 14:17:53 +0200

    Start

--- Itname2/Program.cs
+++ Itname2/Program.cs
@@ -1,27 +1,29 @@
-﻿using System.Collections.Generic;using Itname2;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Itname2;
 
-bool IsEqueal(Point a, Point b)
+bool IsEqual(Point a, Point b)
 {
-    return a.Column == b.Column && a.Row == b.Row;
+    return a.Column == b.Column && a.Row == b.Row; 
 }
 
-
-
-void PrintMathWithPath(string[,] map, List<Point> path)
+void PrintMapWithPath(string[,] map, List<Point> path)
 {
-    Point start = path[0];
-    Point end = path[^1];
-    
-    foreach (Point p in path )
+    Point start = path.First();
+    Point end = path.Last();
+
+    foreach (Point p in path)
     {
-        if (IsEqueal (p.== start))
+        if (IsEqual(p,start))
         {
             map[p.Column, p.Row] = "A";
-        }else if (IsEqueal (p.== end))
+        }
+        else if (IsEqual(p,end))
         {
             map[p.Column, p.Row] = "B";
-        }else 
-
+        }
+        else
         {
             map[p.Column, p.Row] = ".";
         }
@@ -29,68 +31,99 @@ void PrintMathWithPath(string[,] map, List<Point> path)
     new MapPrinter().Print(map);
 }
 
-bool ISndnnfnf(string c)
+bool IsWall(string c)
 {
     return c == "█";
 }
+
 List<Point> GetNeighbourd(string[,] map, Point p)
 {
     List<Point> result = new List<Point>();
+
     int px = p.Column;
     int py = p.Row;
-
-    if (py + 1 < map.GetLength(0) && py + 1 >= 0 && px < map.GetLength(1) && px >= 0 && ISndnnfnf(map[px, py + 1]))
+    
+    if (py + 1 < map.GetLength(1) && py + 1 >= 0 && px < map.GetLength(0) && px >= 0 && !IsWall(map[px, py + 1]))
     {
-        result.Add(new Point(px, py +1 ));
+        result.Add(new Point(px, py + 1 ));
     }
-    if (py - 1 < map.GetLength(0) && py - 1 >= 0 && px < map.GetLength(1) && px >= 0 && ISndnnfnf(map[px, py - 1]))
+    if (py - 1 < map.GetLength(1) && py - 1 >= 0 && px < map.GetLength(0) && px >= 0 && !IsWall(map[px, py - 1]))
     {
         result.Add(new Point(px, py - 1 ));
     }
 
-    if (py < map.GetLength(0) && py >= 0 && px + 1 < map.GetLength(1) && px + 1 >= 0 && ISndnnfnf(map[px + 1, py]))
+    if (py < map.GetLength(1) && py >= 0 && px + 1 < map.GetLength(0) && px + 1 >= 0 && !IsWall(map[px + 1, py]))
     {
         result.Add(new Point(px + 1, py));
     }
 
-    if (py  < map.GetLength(0) && py  >= 0 && px - 1 < map.GetLength(1) && px+ 1 >= 0 && ISndnnfnf(map[px - 1, py ]))
+    if (py < map.GetLength(1) && py >= 0 && px - 1 < map.GetLength(0) && px + 1 >= 0 && !IsWall(map[px - 1, py]))
     {
-        result.Add(new Point(px - 1, py  ));
+        result.Add(new Point(px - 1, py));
+    }
 
     return result;
-    
-}}
-List<Point> Search(string[,] map, Point start, Point end)
-{
-    Queue<Point> fronting = new Queue<Point>();
-    fronting.Enqueue(start);
+}
 
-    while (fronting.Count > 0)
+    List<Point> SearchBfs(string[,] map, Point start, Point end)
+{
+    Queue<Point> frointier = new Queue<Point>();
+    Dictionary<Point, Point?> cameFrom = new Dictionary<Point, Point?>();
+    
+    cameFrom.Add(start, null);
+        
+    frointier.Enqueue(start);
+
+    while (frointier.Count > 0)
     {
-        Point cut  = fronting.Enqueue();
-        if (IsEqueal((cut. == end ))
+        Point cur = frointier.Dequeue();
+
+        if (IsEqual(cur,end))
         {
             break;
         }
-        
+        foreach (Point neighbour in GetNeighbourd(map, cur))
+        {
+            if (cameFrom.TryGetValue(neighbour, out _))
+            {
+                cameFrom.Add(neighbour, cur);
+                frointier.Enqueue(neighbour);
+            }
+        }
     }
+
+    List<Point> path = new List<Point>();
+    Point? current = end;
+
+    while (IsEqual(current.Value, start))
+    {
+        path.Add(current.Value);
+        cameFrom.TryGetValue(current.Value, out current);
+    }
+    path.Add(start);
+
+    path.Reverse();
+    return path;
 }
 
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
     Height = 10,
     Width = 15,
-    Seed = 12312
-    
+    Seed = 7355608
 });
-List<Point> path = new List<Point>(new List<Point>()
-{
-    new Point(0,1 ),
-    new Point (1,0),
-    new Point(2,0),
-    new Point(3,0)
-});
-
 
 string[,] map = generator.Generate();
-new MapPrinter().Print(map);
+
+/*foreach (Point n in GetNeighbourd(map, p))
+{
+    map[n.Column, n.Row] = "N";
+}*/
+Point start = new Point(0, 0);
+Point end = new Point(0, 8);
+List<Point> path = SearchBfs(map, start, end);
+    
+PrintMapWithPath(map,path);
+//PrintMapWithPath(map,path);
+
+//new MapPrinter().Print(map);
